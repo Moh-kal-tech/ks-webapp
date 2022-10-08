735a2484ee489ba2cbc8319f9d16cb1443f6bb52
mohamed kalifa <cecsmohamedkalifa2024@gmail.com>
2022-10-08 13:56:46 +0530

    Add files via upload

---
+++ webapp/script.js
@@ -0,0 +1,17 @@
+
+const userName = 'Harish';
+const pass = 12345;
+
+console.log("Hi");
+function afterClick(){
+    if(document.getElementById('inpMail').value == userName){
+        if(document.getElementById('inpPass').value == pass){
+            alert('Welcome');
+        }
+        else{
+            alert('Your Password is wrong');
+        }
+    }else{
+        alert('Your User Name is wrong');
+    }
+}
